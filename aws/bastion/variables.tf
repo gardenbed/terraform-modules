5b9abd4f075b9c3f54f5c5e25915ce1703a4e413
Milad Irannejad <moorara@users.noreply.github.com>
2021-12-20 13:09:01 -0500

    Refactor aws/bastion module

--- aws/bastion/variables.tf
+++ aws/bastion/variables.tf
@@ -4,11 +4,13 @@
 variable "name" {
   description = "The name of the deployment."
   type        = string
+  nullable    = false
 }
 
 variable "region" {
   description = "The AWS region of the deployment."
   type        = string
+  nullable    = false
 }
 
 # ==================================================< NETWORKING >==================================================
@@ -19,6 +21,7 @@ variable "vpc" {
     id   = string
     cidr = string
   })
+  nullable = false
 }
 
 variable "public_subnets" {
@@ -27,6 +30,7 @@ variable "public_subnets" {
     id   = string
     cidr = string
   }))
+  nullable = false
 }
 
 # ==================================================< BASTION >==================================================
@@ -34,30 +38,29 @@ variable "public_subnets" {
 variable "instance_type" {
   description = "The AWS EC2 instance type for bastion hosts."
   type        = string
+  nullable    = false
   default     = "t2.micro"
 }
 
 variable "ssh_cidrs" {
   description = "A set of trusted CIDR blocks for incoming traffic to bastion hosts."
   type        = set(string)
+  nullable    = false
   default     = [ "0.0.0.0/0" ]
 }
 
-variable "ssh_path" {
-  description = "The path to a directory for SSH config file."
+variable "ssh_public_key_file" {
+  description = "The path to public key file for SSH access to bastion hosts."
   type        = string
-  default     = null
+  nullable    = false
 }
 
-variable "private_key_file" {
-  description = "The path to the SSH private key file for bastion hosts."
-  type        = string
-  default     = null
-}
-
-variable "public_key_file" {
-  description = "The path to the SSH public key file for bastion hosts."
-  type        = string
+variable "ssh_config_file" {
+  description = "If set, an SSH config file will be written next to the private key file."
+  type = object({
+    private_key_file = string
+  })
+  default = null
 }
 
 # ==================================================< TAGS >==================================================
@@ -65,5 +68,5 @@ variable "public_key_file" {
 variable "common_tags" {
   description = "A map of common tags for the resources."
   type        = map(string)
-  default     = {}
+  default     = null
 }
